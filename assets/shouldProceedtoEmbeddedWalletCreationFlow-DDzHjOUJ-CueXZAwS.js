406901f1c15251077f62936dcd4ae98a88918d1a
huseyinaztekintr-ctrl <huseyinaztekintr@gmail.com>
2026-09-25 04:00:52 +0300

    Games: route all approve+commit through fast sendGameTx (MAX approve once); blackjack auto-clears timed-out hands on deal (no forced forfeit)

--- assets/shouldProceedtoEmbeddedWalletCreationFlow-DDzHjOUJ-CueXZAwS.js
+++
@@ -1,1 +0,0 @@
-import{q as o,r as a}from"./privyHost-CoQ14Pw0.js";const n=(r,e)=>o(r,e.ethereum.createOnLogin)||a(r,e.solana.createOnLogin);export{n as a};
---
+++ assets/shouldProceedtoEmbeddedWalletCreationFlow-DDzHjOUJ-Dql-dGwa.js
@@ -0,0 +1,1 @@
+import{q as o,r as a}from"./privyHost-CQ5MxjbC.js";const n=(r,e)=>o(r,e.ethereum.createOnLogin)||a(r,e.solana.createOnLogin);export{n as a};
